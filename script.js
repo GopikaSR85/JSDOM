628b0a0d0b0e420ab87d8ae0228919b7e4d34fe6
GopikaSR85 <gopika.s.r.24@gmail.com>
2025-06-20 11:21:21 +0530

    complete Cartquantity project

---
+++ script.js
@@ -0,0 +1,21 @@
+let newObj={
+    name:"Gopika hi",
+    display:function(){
+        console.log("Hello");
+    }
+}
+console.log("Hi")
+console.time("Hello")
+console.warn("Something wrong ")
+console.error("its error")
+console.table([{name:"Alice"},{name:"bob"}]);
+console.time("loop");
+for(let i=0;i<100000;i++){ }
+console.timeEnd("bye");
+const x=10;
+const y=20;
+const result=eval("x+y");
+console.log(result);
+"use strict";
+let a=10;
+b=20;
