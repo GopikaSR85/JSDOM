f072ab8a664e78e82121d4b2c2c57c561d0c1f2a
GopikaSR85 <gopika.s.r.24@gmail.com>
2025-06-20 14:13:42 +0530

    complete jsons topic

--- json.js
+++ json.js
@@ -1,17 +1,25 @@
-const jsObj={
-    name:"Gopika hlo",
-    age:19,
-    display:function(){
-        console.log("Hello");
-    },
-}
-let jsonObj={
-    "name":"Gopika hlo",
-    "age":19,
-};
-let stringJS=JSON.stringify(jsObj);
-console.log(stringJS);
-console.log(typeof stringJS);
-let normalJS=JSON.parse(jsObj);
-console.log(normalJS);
-console.log(typeof normalJS);
+// const jsObj={
+//     name:"Gopika hlo",
+//     age:19,
+//     display:function(){
+//         console.log("Hello");
+//     },
+// }
+// let jsonObj={
+//     "name":"Gopika hlo",
+//     "age":19,
+// };
+// let stringJS=JSON.stringify(jsObj);
+// console.log(stringJS);
+// console.log(typeof stringJS);
+// let normalJS=JSON.parse(jsObj);
+// console.log(normalJS);
+// console.log(typeof normalJS);
+
+localStorage.setItem("username","Gopika hii");
+let item=localStorage.getItem("username");
+console.log(item);
+localStorage.removeItem("username");
+let item2=localStorage.getItem("username");
+console.log(item2);
+sessionStorage.setItem("username","Gopika hii");
